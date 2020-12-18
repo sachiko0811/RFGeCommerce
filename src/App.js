1c1e0e8ef6f9cddfedb616f8264bb3f19817c538
sachiko1995 <sachikonovel@yahoo.co.jp>
2020-12-18 13:17:38 -0800

    header footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import Header from './components/Header';
+import { Switch, Route } from 'react-router-dom';
+
+// layouts
+import MainLayout from './layouts/MainLayout';
+import HomepageLayout from './layouts/HomepageLayout';
+
 import Homepage from './pages/Homepage/index';
+import Registration from './pages/Registration/index'
 import './default.scss';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Homepage />
-      </div>
+        <Switch>
+          <Route exact 
+          path="/" 
+          // component={Homepage}
+          render={() => (
+            <HomepageLayout>
+            <Homepage />
+            </HomepageLayout>
+          )}/>
+          <Route 
+          path="/registration" 
+          // component={Registration}
+          render={()=> (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}
+          />
+        </Switch>
     </div>
   );
 }
---
+++ src/pages/Registration/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+import './styles.scss';
+
+
+class Registration extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Regi
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default Registration;
